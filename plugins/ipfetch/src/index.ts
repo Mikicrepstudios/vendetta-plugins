7ca636009911d0f67397d28d14d5c4f81160d614
Mikicrep <mikicrepoffical@gmail.com>
2023-05-21 12:57:08 +0200

    3

---
+++ plugins/ipfetch/src/index.ts
@@ -0,0 +1,30 @@
+import { logger } from "@vendetta";
+import Settings from "./Settings";
+
+export default {
+	onLoad: function() {
+	    this.onUnload = commands.registerCommand({
+		execute: (args, ctx) => {
+			sendEphemeralClydeMessage(ctx.channel.id, "Test");
+		} catch (err) {
+			logger.error(err);
+			sendEphemeralClydeMessage(ctx.channel.id, "Command failed to run: " + err.message);
+		},
+		name: "ip",
+		displayName: "ip",
+		description: "Fetch IP address",
+		options: Array.from({length:20}).fill({
+			required: true,
+			type: 3,
+			name: "ipaddress",
+			displayName: "ipaddress",
+			description: "Write IP address you want to fetch",
+			displayDescritpion: "Write IP address you want to fetch",
+		}),
+		applicationId: -1,
+		inputType: 1,
+		type: 1,
+	}),
+
+	settings: Settings,
+}
