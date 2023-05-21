147141298bf0b9ba5952a5629155a17453bddf9e
Mikicrep <mikicrepoffical@gmail.com>
2023-05-21 13:22:08 +0200

    10

--- plugins/ipfetch/src/index.ts
+++ plugins/ipfetch/src/index.ts
@@ -1,33 +1,34 @@
 import Settings from "./Settings";
 
 export default {
-	const { metro, commands, logger } = vendetta;
-	const { sendBotMessage: sendEphemeralClydeMessage } = metro.findByProps("sendBotMessage");
 
 	onLoad: function() {
-	    this.onUnload = commands.registerCommand({
-		execute: (args, ctx) => {
-			try {
-				sendEphemeralClydeMessage(ctx.channel.id, "Test");
-			} catch (err) {
-				logger.error(err);
-				sendEphemeralClydeMessage(ctx.channel.id, "Command failed to run: " + err.message);
-			}
-		},
-		name: "ip",
-		displayName: "ip",
-		description: "Fetch IP address",
-		options: Array.from({length:20}).fill({
-			required: true,
-			type: 3,
-			name: "ipaddress",
-			displayName: "ipaddress",
-			description: "Write IP address you want to fetch",
-			displayDescritpion: "Write IP address you want to fetch",
-		}),
-		applicationId: -1,
-		inputType: 1,
-		type: 1,
+		const { metro, commands, logger } = vendetta;
+		const { sendBotMessage: sendEphemeralClydeMessage } = metro.findByProps("sendBotMessage");
+
+		this.onUnload = commands.registerCommand({
+			execute: (args, ctx) => {
+				try {
+					sendEphemeralClydeMessage(ctx.channel.id, "Test");
+				} catch (err) {
+					logger.error(err);
+					sendEphemeralClydeMessage(ctx.channel.id, "Command failed to run: " + err.message);
+				}
+			},
+			name: "ip",
+			displayName: "ip",
+			description: "Fetch IP address",
+			options: Array.from({length:20}).fill({
+				required: true,
+				type: 3,
+				name: "ipaddress",
+				displayName: "ipaddress",
+				description: "Write IP address you want to fetch",
+				displayDescritpion: "Write IP address you want to fetch",
+			}),
+			applicationId: -1,
+			inputType: 1,
+			type: 1,
 		});
 	
 	},
