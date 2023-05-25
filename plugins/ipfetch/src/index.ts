fb8327e282417712dfbfa11026e3b008ea186eb4
Mikicrep <mikicrepoffical@gmail.com>
2023-05-25 16:07:21 +0200

    34

--- plugins/ipfetch/src/index.ts
+++ plugins/ipfetch/src/index.ts
@@ -16,8 +16,8 @@ export default  {
 			})
 			.catch(error => {
     				console.error('[Mikicrep ipfetch]: error occured: ', error);
-			};
-		}
+			},
+		};
 	
 
 		this.onUnload = commands.registerCommand({
