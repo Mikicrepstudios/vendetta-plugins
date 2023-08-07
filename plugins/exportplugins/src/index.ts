361d21062bdae3e32f124ce853781fc21c1f8fdb
Mikicrep <mikicrepoffical@gmail.com>
2023-08-07 09:30:38 +0200

    42
    - ?

--- plugins/exportplugins/src/index.ts
+++ plugins/exportplugins/src/index.ts
@@ -1,11 +1,11 @@
 import { logger } from "@vendetta";
-import vendetta from "vendetta"
+#import vendetta from "vendetta"
 import Settings from "./Settings";
 
 export default {
     onLoad: () => {
         logger.log("[Mikicrep export plugins]: loaded");
-		logger.log("[Mikicrep export plugins]: " + vendetta.plugins.plugins);
+		logger.log("[Mikicrep export plugins]: " + vendetta.plugins.plugins.Plugin);
     },
     onUnload: () => {
         logger.log("[Mikicrep export plugins]: unloaded");
